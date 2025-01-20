fe85c6bba58fe10dd105c009ffb0c7138440fc21
mtmodesti <marcelomodesti1988@gmail.com>
2025-01-20 11:40:42 -0300

    feat: new create unity form

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideEnvironmentNgxMask(),
+  ],
 };
--- src/app/views/create-unity/create-unity.component.ts
+++ src/app/views/create-unity/create-unity.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { NgxMaskDirective } from 'ngx-mask';
+import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-unity',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIcon,
+    NgxMaskDirective,
+    MatButtonModule,
+  ],
   templateUrl: './create-unity.component.html',
-  styleUrl: './create-unity.component.css'
+  styleUrls: ['./create-unity.component.css'],
 })
 export class CreateUnityComponent {
+  constructor(private router: Router) {}
 
+  onSubmit(userForm: any): void {
+    console.log('Form Values:', userForm.value);
+  }
+  goToDashboard(userForm: any): void {
+    userForm.reset();
+    this.router.navigate(['/dashboard']);
+  }
 }
